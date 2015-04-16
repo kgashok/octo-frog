1cb969fcfb2f799aa73496d3951e1efc8054d460
frog_BDC13F024B <frog@cyber-dojo.org>
2015-04-16 14:09:28 +0000

    7

--- sandbox/hiker.tests.c
+++ sandbox/hiker.tests.c
@@ -9,6 +9,7 @@ static void life_the_universe_and_everything(void)
   //assert (answer(3) == "Fizz");
     assert (strcmp(answer(3), "Fizz") == 0);
 
+    assert (strcmp(answer(5), "Buzz") == 0);
 }
 
 int main(void)
