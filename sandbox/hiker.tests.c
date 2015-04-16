b47996fa6fac0c5d239b20a9d9ffdcfee3a48491
frog_BDC13F024B <frog@cyber-dojo.org>
2015-04-16 14:26:10 +0000

    19

--- sandbox/hiker.tests.c
+++ sandbox/hiker.tests.c
@@ -3,20 +3,19 @@
 #include <stdio.h>
 #include <string.h>
 
-static void life_the_universe_and_everything(void)
+static void fizzbuzzTests(void)
 {
   //assert(answer() == 42);
   //assert (answer(3) == "Fizz");
     assert (strcmp(answer(3), "Fizz") == 0);
 
     assert (strcmp(answer(5), "Buzz") == 0);
-
     assert (strcmp(answer(15), "FizzBuzz") == 0);
 }
 
 int main(void)
 {
-    life_the_universe_and_everything();
+    fizzbuzzTests();
     // green-traffic light pattern...
     puts("All tests passed");
 }
