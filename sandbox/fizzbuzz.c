428e50d6f72c5b70529757143947c4db42c27232
kgashok <ashok@server>
2015-04-19 11:04:23 +0000

    More elegant

--- sandbox/fizzbuzz.c
+++ sandbox/fizzbuzz.c
@@ -14,7 +14,8 @@ char* fizzBuzz(int val)
         strcpy(buf, "Fizz");
     if (val % 5 == 0) 
         strcat(buf, "Buzz");
-    if (val % 3 != 0 && val % 5 != 0)
+    //if (val % 3 != 0 && val % 5 != 0)
+    if (strlen (buf) == 0)
         sprintf(buf, "%d", val);
 
     return buf;
