a0e52fd02d5d92917fc1d58c7d93884583028216
frog_BDC13F024B <frog@cyber-dojo.org>
2015-04-20 00:42:54 +0530

    Duplicate code. WTH?

--- sandbox/fizzbuzz.c
+++ sandbox/fizzbuzz.c
@@ -14,28 +14,6 @@ char* fizzBuzz(int val)
         strcat(buf, "Fizz");
     if (val % 5 == 0) 
         strcat(buf, "Buzz");
-    if (strlen (buf) == 0)
-        sprintf(buf, "%d", val);
-
-    return buf;
-     
-}
-#include "fizzbuzz.h"
-
-#include <stdio.h>
-#include <string.h>
-
-static char buf[30];
-
-char* fizzBuzz(int val)
-{
-    // initialize static variable, it may contain something!
-    memset(buf,0,sizeof(buf));
-
-    if (val % 3 == 0)
-        strcat(buf, "Fizz");
-    if (val % 5 == 0) 
-        strcat(buf, "Buzz");
     if (strlen(buf) == 0)
         sprintf(buf, "%d", val);
 
@@ -49,7 +27,8 @@ char* fizzBuzzSeq(int val)
 {
     memset(sbuf, 0, sizeof(buf));
 
-    for (int i = 1; i <= val; i++) {
+    int i;
+    for (i = 1; i <= val; i++) {
         strcat(sbuf, fizzBuzz(i));
         if (i != val)
             strcat (sbuf, ", ");
