0375a634bea5c2c3442e56d26c209cd6f27ce675
koala_79FE2F8E85 <koala@cyber-dojo.org>
2015-04-19 17:17:16 +0000

    21

--- sandbox/fizzbuzz.c
+++ sandbox/fizzbuzz.c
@@ -11,7 +11,7 @@ char* fizzBuzz(int val)
     memset(buf,0,sizeof(buf));
 
     if (val % 3 == 0)
-        strcpy(buf, "Fizz");
+        strcat(buf, "Fizz");
     if (val % 5 == 0) 
         strcat(buf, "Buzz");
     if (strlen(buf) == 0)
@@ -26,7 +26,6 @@ static char sbuf[1000];
 char* fizzBuzzSeq(int val)
 {
     memset(sbuf, 0, sizeof(buf));
-    //printf ("%d", val);
 
     for (int i = 1; i <= val; i++) {
         strcat(sbuf, fizzBuzz(i));
