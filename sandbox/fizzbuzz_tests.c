ac6a7a91134916b064467664a68ef445573ce525
koala_79FE2F8E85 <koala@cyber-dojo.org>
2015-04-19 17:00:44 +0000

    3

--- sandbox/fizzbuzz_tests.c
+++ sandbox/fizzbuzz_tests.c
@@ -8,6 +8,9 @@ static void fizzbuzzTests(void)
     assert (strcmp(fizzBuzz(3),  "Fizz")     == 0);
     assert (strcmp(fizzBuzz(5),  "Buzz")     == 0);
     assert (strcmp(fizzBuzz(15), "FizzBuzz") == 0);
+
+    assert (strcmp(fizzBuzzSeq(3), "1, 2, Fizz") == 0);
+
 }
 
 int main(void)
