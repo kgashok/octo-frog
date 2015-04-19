609e8186902d58b318a155a6b0e722fca71bd864
koala_79FE2F8E85 <koala@cyber-dojo.org>
2015-04-19 17:24:17 +0000

    22

--- sandbox/fizzbuzz_tests.c
+++ sandbox/fizzbuzz_tests.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
+char st16 = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, 10, 11, Fizz, 13, 14, FizzBuzz, 16";
+
 static void fizzbuzzTests(void)
 {
     assert (strcmp(fizzBuzz(3),  "Fizz")     == 0);
@@ -10,6 +12,8 @@ static void fizzbuzzTests(void)
     assert (strcmp(fizzBuzz(15), "FizzBuzz") == 0);
 
     assert (strcmp(fizzBuzzSeq(3), "1, 2, Fizz") == 0);
+    assert (strcmp(fizzBuzzSeq(16), st16) == 0);
+
 
 }
 
